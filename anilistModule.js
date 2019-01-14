a9a72d4e72a1e082fd337fea7b334cc8cb08eef5
chaojiac <euleriepsilon@gmail.com>
2019-01-14 01:39:14 -0800

    completed logic on updatelist

--- anilistModule.js
+++ anilistModule.js
@@ -72,59 +72,60 @@ const mutation = `mutation($about: String){
     }
 }`
 
-const UpdateMediaListEntries = `mutation($ids: [Int], $status: MediaListStatus, $progress: Int){
-    UpdateMediaListEntries(ids:$ids, status:$status, progress: $progress){
-        status
-        progress
-    }
-}`
 
-const SaveMediaListEntry = `mutation($mediaId: Int, $progress: Int, $status: MediaListStatus){
-    SaveMediaListEntry(mediaId:$mediaId, progress:$progress, status: $status){
+const SaveMediaListEntry = `mutation($mediaId: Int, $progress: Int, $status: MediaListStatus, $listEntryId: Int){
+    SaveMediaListEntry(mediaId:$mediaId, progress:$progress, status: $status, id: $listEntryId){
         mediaId
         progress
         status
     }
 }`
 
-const determineIfInList = async function(response){
-    // If in list, return the corresponding mediaListEntryID and status.
-    if (response.Media.mediaListEntry){
-        return response.Media.mediaListEntry
-    }else {
-        return false
-    }
-}
-
-const updateListEntry = async function(vars){
-    return graphQLClient.request(UpdateMediaListEntries, vars);
-}
 
 const saveListEntry = async function(vars){
     return graphQLClient.request(SaveMediaListEntry, vars);
 }
 
-
+// List updates are handled by this function
+// Expects aniListID and progress (int)
 module.exports.mutation = async (vars) => {
+    var totalEpisodes
     return graphQLClient.request(findAnimeByID, vars)
-        .then(determineIfInList)
+        .then(async (response) => {
+            totalEpisodes = response.Media.episodes;
+            return (response.Media.mediaListEntry) ? response.Media.mediaListEntry : false;
+        })
         .then(async (listEntry) => {
-            var result;
-            if (listEntry != false) 
-                result =  "Watched" ;
-            else {
+            if (listEntry != false) {
+                // If currently in list, as WATCHING
+                if (listEntry.status == "COMPLETED" ){
+                    vars.status = "REPEATING"
+                    vars.listEntryId = listEntry.id
+                    // For whatever reason Anilist API does not take the 'progress' param when setting as REPEATING, so 2 calls are required.
+                    await saveListEntry(vars); 
+                } else if (listEntry.status == "REPEATING"){
+                // EIther completed or repeating
+                    vars.listEntryId = listEntry.id
+                    if (vars.progress == totalEpisodes) {
+                        vars.status = "COMPLETED"
+                    }
+                } else {
+                    vars.status = "CURRENT"
+                    vars.listEntryId = listEntry.id
+                    if (vars.progress == totalEpisodes) {
+                        vars.status = "COMPLETED"
+                    }
+                }
+            } else {
                 vars.status = "CURRENT";
                 vars.mediaId = vars.aniID; 
-                result =  await saveListEntry(vars);
-            }
-            return result
                 
+            }
+            return await saveListEntry(vars);
         } )
 };
 
 module.exports.query = async (vars) => {
     
-    return graphQLClient.request(findAnimeByID, vars).then(data => {
-        return data
-    })
+    return graphQLClient.request(findAnimeByID, vars)
 }
