9e5b61d606b682ad57482f0ce5ae928de3db9228
Jack Chao <euleriepsilon@gmail.com>
2019-01-19 16:14:47 -0800

    Completed pipeline with sns/sqs support

--- src/anilist-updater.js
+++ src/anilist-updater.js
@@ -27,4 +27,34 @@ app.post('/search', async (req, res) => {
 })
 
 
-module.exports.index = serverless(app);
+const apiHandler = serverless(app)
+
+// module.exports.index = serverless(app)
+module.exports.index = async (event, context, cb) => {
+  if (event.Records){
+      // from sqs
+      for (const record of event.Records){
+        const recBody = JSON.parse(record.body)
+        switch(recBody.Subject){
+          case 'ANILIST_UPDATE': {
+            const vars = JSON.parse(recBody.Message)
+            console.log(vars)
+
+            const update = await anilistModule.mutation({
+              aniID: vars.ani_id,
+              progress: vars.episode
+            });
+            console.log(update);
+            
+            
+            
+          }break;
+        }
+      }
+      cb(null, "Done")
+       
+  } else {
+      // from api gateway
+      return await apiHandler(event, context)
+  }
+}
--- src/id-converter.js
+++ src/id-converter.js
@@ -1,6 +1,45 @@
 const tvdb = require('../lib/tvdb')
+const anilist = require('../lib/anilistModule')
 
-module.exports.index = async (event, context, cb) => {
-    const res = await tvdb.getSeriesSeaonEpisode(event.tvdb_id, event.season, event.episode)
-    cb(null, res)
-}
+const AWS = require('aws-sdk')
+const express = require('express')
+const serverless = require('serverless-http')
+const bodyParser = require('body-parser')
+const app = express()
+const SNS = new AWS.SNS()
+
+app.use(bodyParser.json());
+
+app.post('/', async (req, res) => {
+    const tvdb_query_res = await tvdb.getSeriesSeaonEpisode(req.body.tvdb_id, req.body.season, req.body.episode)
+    const airedArr = tvdb_query_res[0].firstAired.split("-")
+    const queryRes = await anilist.search({
+        searchString: req.body.title,
+        dateStarted: airedArr[0].concat(airedArr[1]).concat("%%")
+    })
+    
+    
+    var params = {
+        Message: JSON.stringify({
+            ani_id: queryRes.Animes.media[0].id,
+            title: req.body.title,
+            season: req.body.season,
+            episode: req.body.episode
+        }), 
+        Subject: "ANILIST_UPDATE",
+        TopicArn: process.env.topicArn
+    };
+    const sns_res = await SNS.publish(params).promise()
+    res.send({anilistId: queryRes.Animes.media[0].id, snsReqId: sns_res.ResponseMetadata.RequestId})
+})
+
+
+
+module.exports.index = serverless(app)
+// module.exports.index = async (event, context, err) => {
+//     if (event.Records){
+//         // from sqs
+//     } else {
+//         // from api gateway
+//     }
+// }
